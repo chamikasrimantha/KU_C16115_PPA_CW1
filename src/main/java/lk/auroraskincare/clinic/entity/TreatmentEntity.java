3e5e9cc7f78e4eea6a9d7c4d6a351368b34aaddf
chamikasrimantha <chamikasrimantha2002@gmail.com>
2024-10-24 11:22:56 +0530

    unused imports removed

--- src/main/java/lk/auroraskincare/clinic/entity/TreatmentEntity.java
+++ src/main/java/lk/auroraskincare/clinic/entity/TreatmentEntity.java
@@ -1,20 +1,13 @@
 package lk.auroraskincare.clinic.entity;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
